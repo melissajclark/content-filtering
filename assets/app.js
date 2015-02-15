7719c25a430ac53acf3fbed284266d5f3d7166f1
Melissa Jean Clark <melissa@melissajclark.ca>
2015-02-15 16:21:23 -0500

    working on something - the actvie remove works

--- assets/app.js
+++ assets/app.js
@@ -55,8 +55,10 @@ filterApp.init = function() { // this function holds everything to start the app
 		// finds the value of the user's selection (aka the desired type to view)
 		filterApp.sortChoice = $(this).find(":selected").val();	
 
-		// gets the data-type from each item and changes the colour based on selection
-		var filterSelected = $(".filterable section[data-type='" + filterApp.sortChoice + "'").find(filterApp.sortChoice);
+		// gets the data-type from each item 
+		$(".filterable section[data-type='" + filterApp.sortChoice + "']").removeClass("active");
+
+		console.log("filterApp.sortChoice = "+ filterApp.sortChoice);
 
 		// console.log(filterApp.sort);
 

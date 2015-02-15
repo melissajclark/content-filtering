db7e097468367a3b3b6151059ada58c4a16a39aa
Melissa Jean Clark <melissa@melissajclark.ca>
2015-02-14 23:29:01 -0500

    updated selectors in click function - earlier they all mapped to typeOne

--- assets/app.js
+++ assets/app.js
@@ -14,28 +14,28 @@ var filterApp = "";
 	}); // end click function
 
 	// filters type2 content
-	filterApp.typeOne = $("a.typeOneFilter").on("click", function(){
+	filterApp.typeOne = $("a.typeTwoFilter").on("click", function(){
 		console.log("hi");
 		$(".type2").toggleClass("filterableItemHidden");
 		$(".type2").toggleClass("filterableItem");
 	}); // end click function
 
 	// filters type3 content
-	filterApp.typeOne = $("a.typeOneFilter").on("click", function(){
+	filterApp.typeOne = $("a.typeThreeFilter").on("click", function(){
 		console.log("hi");
 		$(".type3").toggleClass("filterableItemHidden");
 		$(".type3").toggleClass("filterableItem");
 	}); // end click function
 
 		// filters type4 content
-	filterApp.typeOne = $("a.typeOneFilter").on("click", function(){
+	filterApp.typeOne = $("a.typeFourFilter").on("click", function(){
 		console.log("hi");
 		$(".type4").toggleClass("filterableItemHidden");
 		$(".type4").toggleClass("filterableItem");
 	}); // end click function
 
 	// filters type1 content
-	filterApp.typeOne = $("a.typeOneFilter").on("click", function(){
+	filterApp.typeOne = $("a.typeFiveFilter").on("click", function(){
 		console.log("hi");
 		$(".type5").toggleClass("filterableItemHidden");
 		$(".type5").toggleClass("filterableItem");

6d6c12315ebe8c4789a6d0ed1a8365fbfa0b1342
Melissa Jean Clark <melissa@melissajclark.ca>
2015-02-14 23:36:00 -0500

    renamed variables so they are all unique

--- assets/app.js
+++ assets/app.js
@@ -14,30 +14,32 @@ var filterApp = "";
 	}); // end click function
 
 	// filters type2 content
-	filterApp.typeOne = $("a.typeTwoFilter").on("click", function(){
+	filterApp.typeTwo = $("a.typeTwoFilter").on("click", function(){
 		console.log("hi");
 		$(".type2").toggleClass("filterableItemHidden");
 		$(".type2").toggleClass("filterableItem");
 	}); // end click function
 
 	// filters type3 content
-	filterApp.typeOne = $("a.typeThreeFilter").on("click", function(){
+	filterApp.typeThree = $("a.typeThreeFilter").on("click", function(){
 		console.log("hi");
 		$(".type3").toggleClass("filterableItemHidden");
 		$(".type3").toggleClass("filterableItem");
 	}); // end click function
 
 		// filters type4 content
-	filterApp.typeOne = $("a.typeFourFilter").on("click", function(){
+	filterApp.typeFour = $("a.typeFourFilter").on("click", function(){
 		console.log("hi");
 		$(".type4").toggleClass("filterableItemHidden");
 		$(".type4").toggleClass("filterableItem");
 	}); // end click function
 
 	// filters type1 content
-	filterApp.typeOne = $("a.typeFiveFilter").on("click", function(){
+	filterApp.typeFive = $("a.typeFiveFilter").on("click", function(){
 		console.log("hi");
 		$(".type5").toggleClass("filterableItemHidden");
 		$(".type5").toggleClass("filterableItem");
 	}); // end click function
+
+	fi
 }); // end doc ready

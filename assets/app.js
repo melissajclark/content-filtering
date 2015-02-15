7b5f8ce4e9a6b734384acbe280cc35c0f06ab677
Melissa Jean Clark <melissa@melissajclark.ca>
2015-02-14 23:14:18 -0500

    all working

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
 
+// Empty object for all code to live on
 
-	$("a.typeOneFilter").on("click", function(){
-		$(".type1").remove();
+var filterApp = "";
+
+// ------------------------------------
+
+	// filters type1 content
+	filterApp.typeOne = $("a.typeOneFilter").on("click", function(){
+		console.log("hi");
+		$(".filterableItem.type1").toggleClass("hiddenByFilter");
 	});
 
 

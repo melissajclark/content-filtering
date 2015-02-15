91f8ba3732cb83e07b680b4915aad178c6bc538e
Melissa Jean Clark <melissa@melissajclark.ca>
2015-02-15 14:25:01 -0500

    added type1 to legend

--- assets/app.js
+++ assets/app.js
@@ -51,7 +51,7 @@ filterApp.init = function() { // this function holds everything to start the app
 			// hides all other types
 			$(".type2, .type4, .type5, .type3").toggleClass("filterableItemHidden");
 			$(".type2, .type4, .type5, .type3").toggleClass("filterableItem");
-			console.log("type1 selected!");
+			$("span.currentContent").append("Type1");
 
 		} else if (filterApp.sortChoice === "type2") {
 			$(".type2").toggleClass("filterableItemHidden");

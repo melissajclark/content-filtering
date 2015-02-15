66e3e0460305573d23075c556600a882db295681
Melissa Jean Clark <melissa@melissajclark.ca>
2015-02-14 23:27:47 -0500

    set up selectors for all items

--- assets/app.js
+++ assets/app.js
@@ -13,5 +13,31 @@ var filterApp = "";
 		$(".type1").toggleClass("filterableItem");
 	}); // end click function
 
+	// filters type2 content
+	filterApp.typeOne = $("a.typeOneFilter").on("click", function(){
+		console.log("hi");
+		$(".type2").toggleClass("filterableItemHidden");
+		$(".type2").toggleClass("filterableItem");
+	}); // end click function
 
+	// filters type3 content
+	filterApp.typeOne = $("a.typeOneFilter").on("click", function(){
+		console.log("hi");
+		$(".type3").toggleClass("filterableItemHidden");
+		$(".type3").toggleClass("filterableItem");
+	}); // end click function
+
+		// filters type4 content
+	filterApp.typeOne = $("a.typeOneFilter").on("click", function(){
+		console.log("hi");
+		$(".type4").toggleClass("filterableItemHidden");
+		$(".type4").toggleClass("filterableItem");
+	}); // end click function
+
+	// filters type1 content
+	filterApp.typeOne = $("a.typeOneFilter").on("click", function(){
+		console.log("hi");
+		$(".type5").toggleClass("filterableItemHidden");
+		$(".type5").toggleClass("filterableItem");
+	}); // end click function
 }); // end doc ready

f5c0484f3af4aaa0e04b451cb3db54d37b5b0c34
“Melissa” <melissa@melissajclark.ca>
2015-02-22 17:20:10 -0500

    duplicated again for colors

--- assets/app.js
+++ assets/app.js
@@ -44,6 +44,7 @@ filterApp.init = function() { // this function holds everything to start the app
 
 	});  // ============ End function that listens on click & evaluates filterApp.dataAttr[0] (Types)
 
+
 	// ============ Function that listens on click & evaluates filterApp.dataAttr[1] (Shapes)
 
 	$('ul.filterNavShapes a.filterControl').on('click',function(){ 
@@ -70,7 +71,36 @@ filterApp.init = function() { // this function holds everything to start the app
 			$('li span.currentChoice').html(filterApp.dataName[1] + ': ' + filterApp.userSelection);
 		}
 
-	});  // ============ End function that listens on click & evaluates filterApp.dataAttr[1] (Shapes)
+	});  // ============ End function that listens on click & evaluates filterApp.dataAttr[1] (Shapes)	
+
+
+	// ============ Function that listens on click & evaluates filterApp.dataAttr[2] (Colors)
+
+	$('ul.filterNavColors a.filterControl').on('click',function(){ 
+
+		// finds the value of the user's selection (aka the desired shape to view)
+		filterApp.userSelection = $(this).text();
+
+		// displays legend after user clicks on a filter link
+		$('section.filterResultsCurrent').show(); 
+
+		// finds items NOT matching user's selection and hides them
+		$('.filterableItem' + '[' + filterApp.dataAttr[2] + ']').not('[' + filterApp.dataAttr[2] + '="' + filterApp.userSelection + '"]').css('background', 'grey');
+		console.log('hiding: [' + filterApp.dataAttr[2] + '="' + filterApp.userSelection + '"] !');
+
+		//finds items matching user's selection and shows them
+		$('.filterableItem' + '[' + filterApp.dataAttr[2] + ']').filter('[' + filterApp.dataAttr[2] + '="' + filterApp.userSelection + '"]').css('background', 'yellow');
+
+		// hides legend if "all" is selected + shows all items when all is selected
+		if (filterApp.userSelection === 'all') {
+			$('section.filterResultsCurrent').hide();
+			$('section.filterableItem').show();
+		} else {
+			$('section.filterResultsCurrent').show();
+			$('li span.currentChoice').html(filterApp.dataName[2] + ': ' + filterApp.userSelection);
+		}
+
+	});  // ============ End function that listens on click & evaluates filterApp.dataAttr[2] (Colors)
 
 }; // end filterApp.init
 

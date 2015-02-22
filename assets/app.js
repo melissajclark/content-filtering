deb45a02d93a56bb5dd57000ee786afb2da25cab
“Melissa” <melissa@melissajclark.ca>
2015-02-22 16:52:10 -0500

    updated selector to focus on filterNavTypes

--- assets/app.js
+++ assets/app.js
@@ -22,7 +22,7 @@ filterApp.init = function() { // this function holds everything to start the app
 
 	// ============ Function that listens on click & evaluates type data
 
-	$('a.filterControl').on('click',function(){ 
+	$('ul.filterNavTypes a.filterControl').on('click',function(){ 
 
 		// finds the value of the user's selection (aka the desired shape to view)
 		filterApp.userSelection = $(this).text();

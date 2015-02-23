5c4fd31061c6b3d1a7a3a019a4810b2fb82adcf7
“Melissa” <melissa@melissajclark.ca>
2015-02-22 22:04:19 -0500

    removed "active" on divs - shouldnt have been there

--- assets/app.js
+++ assets/app.js
@@ -34,10 +34,10 @@ filterApp.init = function() { // this function holds everything to start the app
 		$('section.filterResultsCurrent').show(); 
 
 		// finds items NOT matching user's selection and hides them
-		$('.filterableItem' + '[' + filterApp.dataAttr[0] + ']').not('[' + filterApp.dataAttr[0] + '="' + filterApp.userSelection + '"]').removeClass('active').hide();
+		$('.filterableItem' + '[' + filterApp.dataAttr[0] + ']').not('[' + filterApp.dataAttr[0] + '="' + filterApp.userSelection + '"]').hide();
 
 		//finds items matching user's selection and shows them
-		$('.filterableItem' + '[' + filterApp.dataAttr[0] + ']').filter('[' + filterApp.dataAttr[0] + '="' + filterApp.userSelection + '"]').addClass('active').show();
+		$('.filterableItem' + '[' + filterApp.dataAttr[0] + ']').filter('[' + filterApp.dataAttr[0] + '="' + filterApp.userSelection + '"]').show();
 
 		// hides legend if "all" is selected + shows all items when all is selected
 		if (filterApp.userSelection === 'all') {

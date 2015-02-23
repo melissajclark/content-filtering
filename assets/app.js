dd885ba1cdc5db1f674ac7ce1cab30c20d5c1f26
Melissa Jean Clark <melissa@melissajclark.ca>
2015-02-23 10:36:24 -0500

    fadeOutRight on hide, fadeInLeft on show

--- assets/app.js
+++ assets/app.js
@@ -33,10 +33,10 @@ filterApp.init = function() { // this function holds everything to start the app
 		$('section.filterResultsCurrent').show(); 
 
 		// finds items NOT matching user's selection and hides them
-		$('.filterableItem' + '[' + filterApp.dataAttr[0] + ']').not('[' + filterApp.dataAttr[0] + '="' + filterApp.userSelection + '"]').hide();
+		$('.filterableItem' + '[' + filterApp.dataAttr[0] + ']').not('[' + filterApp.dataAttr[0] + '="' + filterApp.userSelection + '"]').addClass('fadeOutRight').hide();
 
 		//finds items matching user's selection and shows them
-		$('.filterableItem' + '[' + filterApp.dataAttr[0] + ']').filter('[' + filterApp.dataAttr[0] + '="' + filterApp.userSelection + '"]').addClass('fadeOutRight').show();
+		$('.filterableItem' + '[' + filterApp.dataAttr[0] + ']').filter('[' + filterApp.dataAttr[0] + '="' + filterApp.userSelection + '"]').addClass('fadeInLeft').show();
 
 		// hides legend if "all" is selected + shows all items when all is selected
 		if (filterApp.userSelection === 'all') {
@@ -70,10 +70,10 @@ filterApp.init = function() { // this function holds everything to start the app
 		$('section.filterResultsCurrent').show(); 
 
 		// finds items NOT matching user's selection and hides them
-		$('.filterableItem' + '[' + filterApp.dataAttr[1] + ']').not('[' + filterApp.dataAttr[1] + '="' + filterApp.userSelection + '"]').hide();
+		$('.filterableItem' + '[' + filterApp.dataAttr[1] + ']').not('[' + filterApp.dataAttr[1] + '="' + filterApp.userSelection + '"]').addClass('fadeOutRight').hide();
 
 		//finds items matching user's selection and shows them
-		$('.filterableItem' + '[' + filterApp.dataAttr[1] + ']').filter('[' + filterApp.dataAttr[1] + '="' + filterApp.userSelection + '"]').addClass('fadeOutRight').show();
+		$('.filterableItem' + '[' + filterApp.dataAttr[1] + ']').filter('[' + filterApp.dataAttr[1] + '="' + filterApp.userSelection + '"]').addClass('fadeInLeft').show();
 
 		// hides legend if "all" is selected + shows all items when all is selected
 		if (filterApp.userSelection === 'all') {
@@ -107,10 +107,10 @@ filterApp.init = function() { // this function holds everything to start the app
 		$('section.filterResultsCurrent').show(); 
 
 		// finds items NOT matching user's selection and hides them
-		$('.filterableItem' + '[' + filterApp.dataAttr[2] + ']').not('[' + filterApp.dataAttr[2] + '="' + filterApp.userSelection + '"]').hide();
+		$('.filterableItem' + '[' + filterApp.dataAttr[2] + ']').not('[' + filterApp.dataAttr[2] + '="' + filterApp.userSelection + '"]').addClass('fadeOutRight').hide();
 
 		//finds items matching user's selection and shows them
-		$('.filterableItem' + '[' + filterApp.dataAttr[2] + ']').filter('[' + filterApp.dataAttr[2] + '="' + filterApp.userSelection + '"]').addClass('fadeOutRight').show();
+		$('.filterableItem' + '[' + filterApp.dataAttr[2] + ']').filter('[' + filterApp.dataAttr[2] + '="' + filterApp.userSelection + '"]').addClass('fadeInLeft').show();
 
 		// hides legend if "all" is selected + shows all items when all is selected
 		if (filterApp.userSelection === 'all') {

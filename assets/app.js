cda348e2283144710e0fad14dfb50ae8843641db
Melissa Jean Clark <melissa@melissajclark.ca>
2015-03-07 18:33:39 -0500

    replaced all Language with Work

--- assets/app.js
+++ assets/app.js
@@ -36,8 +36,8 @@ $(filterApp.selector[2]).addClass("animated");
 
 		// finds the value of the user's selection (aka the desired content to view)
 		filterApp.userSourceSelection = $(filterApp.selector[0]).val();
-		filterApp.userLanguageSelection = $(filterApp.selector[1]).val();
-		console.log(filterApp.userLanguageSelection + filterApp.userSourceSelection);
+		filterApp.userWorkSelection = $(filterApp.selector[1]).val();
+		console.log(filterApp.userWorkSelection + filterApp.userSourceSelection);
 
 		/**
 		*
@@ -45,47 +45,47 @@ $(filterApp.selector[2]).addClass("animated");
 		*
 		**/
 		
-		// checks if Source = all and language = all
-		if (filterApp.userSourceSelection === filterApp.values[1] && filterApp.userLanguageSelection === filterApp.values[1] || filterApp.userSourceSelection === filterApp.values[3] && filterApp.userLanguageSelection === filterApp.values[3]) {
-			console.log(filterApp.values[0] + 'or' + filterApp.values[2] + ' all Source & languages selected');
+		// checks if Source = all and Work = all
+		if (filterApp.userSourceSelection === filterApp.values[1] && filterApp.userWorkSelection === filterApp.values[1] || filterApp.userSourceSelection === filterApp.values[3] && filterApp.userWorkSelection === filterApp.values[3]) {
+			console.log(filterApp.values[0] + 'or' + filterApp.values[2] + ' all Source & Works selected');
 
 			$(filterApp.selector[2]).show();
 
-		// checks if Source = all but languages do not = all
-		} else if (filterApp.userSourceSelection === filterApp.values[1] && filterApp.userLanguageSelection != filterApp.values[1] || filterApp.userSourceSelection === filterApp.values[3] && filterApp.userLanguageSelection != filterApp.values[3]) {
-			console.log(filterApp.values[0] + 'or' + filterApp.values[2] + ' All Source selected But language != All');
+		// checks if Source = all but Works do not = all
+		} else if (filterApp.userSourceSelection === filterApp.values[1] && filterApp.userWorkSelection != filterApp.values[1] || filterApp.userSourceSelection === filterApp.values[3] && filterApp.userWorkSelection != filterApp.values[3]) {
+			console.log(filterApp.values[0] + 'or' + filterApp.values[2] + ' All Source selected But Work != All');
 
 			// finds items NOT matching user's selection and hides them
-			// selection: Source: all | Language != all
-			$(filterApp.selector[2] + '[' + filterApp.dataAttr[1] + ']').not('[' + filterApp.dataAttr[1] + '="' + filterApp.userLanguageSelection + '"]').addClass(filterApp.transition[0]).hide();
+			// selection: Source: all | Work != all
+			$(filterApp.selector[2] + '[' + filterApp.dataAttr[1] + ']').not('[' + filterApp.dataAttr[1] + '="' + filterApp.userWorkSelection + '"]').addClass(filterApp.transition[0]).hide();
 			
 			//finds items matching user's selection and shows them
-			// selection: Source: all | Language != all
-			$(filterApp.selector[2] + '[' + filterApp.dataAttr[1] + ']').filter('[' + filterApp.dataAttr[1] + '="' + filterApp.userLanguageSelection + '"]').addClass(filterApp.transition[1]).show();
+			// selection: Source: all | Work != all
+			$(filterApp.selector[2] + '[' + filterApp.dataAttr[1] + ']').filter('[' + filterApp.dataAttr[1] + '="' + filterApp.userWorkSelection + '"]').addClass(filterApp.transition[1]).show();
 
-		// checks is languages = all, but Source does not equal all
-		} else if (filterApp.userSourceSelection != filterApp.values[1] && filterApp.userLanguageSelection === filterApp.values[1] || filterApp.userSourceSelection != filterApp.values[3] && filterApp.userLanguageSelection === filterApp.values[3]) {
-			console.log(filterApp.values[0] + 'or' + filterApp.values[2] + ' All languages selected But Source != All'); 
+		// checks is Works = all, but Source does not equal all
+		} else if (filterApp.userSourceSelection != filterApp.values[1] && filterApp.userWorkSelection === filterApp.values[1] || filterApp.userSourceSelection != filterApp.values[3] && filterApp.userWorkSelection === filterApp.values[3]) {
+			console.log(filterApp.values[0] + 'or' + filterApp.values[2] + ' All Works selected But Source != All'); 
 
 			// finds items NOT matching user's selection and hides them
-			// selection: Source = !all | Language = all
+			// selection: Source = !all | Work = all
 			$(filterApp.selector[2] + '[' + filterApp.dataAttr[0] + ']').not('[' + filterApp.dataAttr[0] + '="' + filterApp.userSourceSelection + '"]').addClass(filterApp.transition[0]).hide();
 			
 			//finds items matching user's selection and shows them
-			// selection: Source = !all | Language = all
+			// selection: Source = !all | Work = all
 			$(filterApp.selector[2] + '[' + filterApp.dataAttr[0] + ']').filter('[' + filterApp.dataAttr[0] + '="' + filterApp.userSourceSelection + '"]').addClass(filterApp.transition[1]).show();
 
-		// checks is both Source & language do not equal all
-		}  else if (filterApp.userSourceSelection != filterApp.values[1] && filterApp.userLanguageSelection != filterApp.values[1] || filterApp.userSourceSelection != filterApp.values[3] && filterApp.userLanguageSelection != filterApp.values[3]) {
+		// checks is both Source & Work do not equal all
+		}  else if (filterApp.userSourceSelection != filterApp.values[1] && filterApp.userWorkSelection != filterApp.values[1] || filterApp.userSourceSelection != filterApp.values[3] && filterApp.userWorkSelection != filterApp.values[3]) {
 			console.log(filterApp.values[0] + 'or' + filterApp.values[2] + ' Source & video both do not equal all'); 
 
 			// finds items NOT matching user's selection and hides them
-			// selection: Source = !all | Language = !all
-			$(filterApp.selector[2] + '[' + filterApp.dataAttr[0] + ']' + '[' + filterApp.dataAttr[1] + ']').not('[' + filterApp.dataAttr[0] + '="' + filterApp.userSourceSelection + '"]' + '[' + filterApp.dataAttr[1] + '="' + filterApp.userLanguageSelection + '"]').addClass(filterApp.transition[0]).hide();
+			// selection: Source = !all | Work = !all
+			$(filterApp.selector[2] + '[' + filterApp.dataAttr[0] + ']' + '[' + filterApp.dataAttr[1] + ']').not('[' + filterApp.dataAttr[0] + '="' + filterApp.userSourceSelection + '"]' + '[' + filterApp.dataAttr[1] + '="' + filterApp.userWorkSelection + '"]').addClass(filterApp.transition[0]).hide();
 			
 			//finds items matching user's selection and shows them
-			// selection: Source = !all | Language = !all
-			$(filterApp.selector[2] + '[' + filterApp.dataAttr[0] + ']' + '[' + filterApp.dataAttr[1] + ']').filter('[' + filterApp.dataAttr[0] + '="' + filterApp.userSourceSelection + '"]' + '[' + filterApp.dataAttr[1] + '="' + filterApp.userLanguageSelection + '"]').addClass(filterApp.transition[1]).show();
+			// selection: Source = !all | Work = !all
+			$(filterApp.selector[2] + '[' + filterApp.dataAttr[0] + ']' + '[' + filterApp.dataAttr[1] + ']').filter('[' + filterApp.dataAttr[0] + '="' + filterApp.userSourceSelection + '"]' + '[' + filterApp.dataAttr[1] + '="' + filterApp.userWorkSelection + '"]').addClass(filterApp.transition[1]).show();
 
 		} 
 
